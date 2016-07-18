9bd2b0d9beffaa3ea876f09144789c7a17ee032c
Ivan@prog1dev <ivan.filenko@protonmail.com>
2016-07-18 20:33:47 +0300

    [~] wrapped status_controller into a module

--- app/controllers/project_status/status_controller.rb
+++ app/controllers/project_status/status_controller.rb
@@ -1,17 +1,19 @@
-class StatusController < ::ApplicationController
+module ProjectStatus
+  class StatusController < ApplicationController
 
-  before_action :check_access_key
+    before_action :check_access_key
 
-  def index
-    render json: {
-        git_info:     ProjectStatus::GitInfo.generate,
-        project_info: ProjectStatus::ProjectInfo.generate,
-        started_at:   Time.now
-    }
-  end
+    def index
+      render json: {
+          git_info:     ProjectStatus::GitInfo.generate,
+          project_info: ProjectStatus::ProjectInfo.generate,
+          started_at:   Time.now
+      }
+    end
 
-  protected
+    protected
 
-  def check_access_key
+    def check_access_key
+    end
   end
 end
